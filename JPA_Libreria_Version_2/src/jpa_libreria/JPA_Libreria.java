e4423876ec248b1bc3fdae5151b3b8d2853b1afa
Ivan <becerraivan79@gmail.com>
2022-06-27 19:43:47 -0300

    Ejercisio, commit 1

---
+++ JPA_Libreria_Version_2/src/jpa_libreria/JPA_Libreria.java
@@ -0,0 +1,20 @@
+/*
+ */
+package jpa_libreria;
+
+/**
+ *
+ * @author ivan
+ */
+public class JPA_Libreria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu classMenu = new Menu();
+        //classMenu.Principal();
+        
+    }
+    
+}
